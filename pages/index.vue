af32e33e99dc2e2f779f51351792ce6e1fba7224
mikaazui <adhika725@gmail.com>
2024-01-10 12:00:22 +0700

    start using vue

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div>
+      <div v-if="showContent">
+        <h1>{{ title }}</h1>
+        <p>{{ content }}</p>
+        <p>{{ name }} - {{ age }}</p>
+        
+        <button v-on:click="age--">decrease age</button>
+        <button v-on:click="age++">increase age</button>
+        <br/>
+        <button v-on:click="title = 'ini title'">
+            click here to change title back
+        </button>
+        <button v-on:click="title = 'something else'">
+            click here to change title
+        </button>
+        <br/>
+    </div>
+      <button v-on:click="toggleShowContent">
+        <span v-if="showContent"> hide content </span>
+        <span v-else> show content </span>
+    </button>
+    <p v-show="!showContent">content hided</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showContent: true,
+      title: "ini title",
+      content: "ini content",
+      name: "john doe",
+      age: 20,
+      count: ref(0),
+    };
+  },
+  methods: {
+    toggleShowContent() {
+      this.showContent = !this.showContent;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
