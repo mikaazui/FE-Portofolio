44da369ddda527ba768bcbe826a77d7b73b191c7
mikaazui <adhika725@gmail.com>
2024-01-12 11:08:16 +0700

    done lesson 1 {modal vue}

--- pages/components/Modal.vue
+++ pages/components/Modal.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="Backdrop" @click="closeModal">
-    <div class="Modal" :class="{SaleModal: theme === 'sale' }">
-      <h1 class=" inline-block font-semibold text-3xl pb-3">{{header}}</h1>
-      <p class="text-inherit pb-4 border-b-2">{{text}}</p>
+    <div class="Modal" :class="{SaleModal: theme === 'sale'} ">
+      <slot>Default Content</slot>
+      <!-- <h1 class=" inline-block font-semibold text-3xl pb-3">{{header}}</h1>
+      <p class="text-inherit pb-4 border-b-2">{{text}}</p> -->
+      <div>
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['header', 'text', 'theme'],
+  props: ['theme'],
   methods: {
     closeModal(){
       this.$emit('close')
