b306f64dd05bbc49469525eee5e926414ffb2d23
mikaazui <adhika725@gmail.com>
2024-01-12 11:44:15 +0700

    created first element

--- pages/components/block.vue
+++ pages/components/block.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    
+  <div class="w-[400px] h-[200px] rounded-md bg-slate-500 text-white flex justify-center items-center m-[40px]">
+    click me
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
+export default {
+  props: ['delay']
+}
 
 </script>
 
--- pages/less2.vue
+++ pages/less2.vue
@@ -1,20 +1,18 @@
 <template>
-  <container class="min-h-screen p-16">
-    <div class="flex flex-col items-center gap-3">
+    <div class="min-h-screen flex flex-col items-center gap-3">
       <h1 class="font-semibold text-5xl text-center text-slate-600">
         Reaction Timer
       </h1>
-      <div>
         <button @click="start" class="bg-slate-300 rounded-md px-5 py-1.5">play</button>
-      </div>
+      <block v-if="isPlaying" :delay="delay"/>
     </div>
-  </container>
 </template>
 
 <script>
+import block from "../pages/components/block.vue"
 export default {
-  name: "less1",
-  components: { },
+  name: "less2",
+  components: { block },
   data () {
     return {
       isPlaying: true,
