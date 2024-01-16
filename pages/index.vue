a1e40f9e2e2d7bfec73fa0a00fc48497302ac9b4
mikaazui <adhika725@gmail.com>
2024-01-16 12:03:42 +0700

    configuring drawer

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,59 @@
 <template>
   <div class="min-h-screen flex">
     <!-- MAIN CONTENT -->
-    <div class="bg-slate-600 min-h-screen grow grid grid-cols-10">
+    <div class="min-h-screen grow grid grid-cols-10">
       <!-- PROFILE CARD -->
-      <div class="col-span-10 md:col-span-3 bg-orange-400">
+      <div class="col-span-10 md:col-span-3">
         <div class="md:fixed">content FIXED medium</div>
       </div>
       <!-- SECTIONS -->
-      <div class="col-span-10 md:col-span-7 bg-orange-500">
-        <div class="min-h-screen bg-orange-600"></div>
-        <div class="min-h-screen bg-orange-700"></div>
-        <div class="min-h-screen bg-orange-800"></div>
-        <div class="min-h-screen bg-orange-900"></div>
+      <div class="col-span-10 md:col-span-7">
+        <div class="min-h-screen"></div>
+        <div class="min-h-screen"></div>
+        <div class="min-h-screen"></div>
+        <div class="min-h-screen"></div>
       </div>
     </div>
     <!-- SIDEMENU -->
-    <div class="bg-slate-900 min-h-screen w-28 flex-none max-md:hidden">
+    <div class="min-h-screen w-28 flex-none max-md:hidden">
       <div class="fixed min-h-screen flex items-center">
         <div
-          class="border-2 border-neutral-600 p-4 rounded-full flex flex-col gap-2"
+          class="border-2 border-neutral-600 p-5 rounded-full flex flex-col gap-5"
         >
           <div class="tooltip tooltip-left" data-tip="Home">
-            <LucideHome :size="22" class="text-accent" />
+            <LucideHome :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="About">
-            <LucideUserRound :size="22" class="text-accent" />
+            <LucideUserRound :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="Resume">
-            <LucideBriefcase :size="22" class="text-accent" />
+            <LucideBriefcase :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="Service">
-            <LucideLibraryBig :size="22" class="text-accent" />
+            <LucideLibraryBig :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="Skills">
-            <LucideBlocks :size="22" class="text-accent" />
+            <LucideBlocks :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="Potofolio">
-            <LucideNotebook-tabs :size="22" class="text-accent" />
+            <LucideNotebook-tabs :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="Testimonials">
-            <LucideMessageSquareText :size="22" class="text-accent" />
+            <LucideMessageSquareText :size="18" class="text-accent" />
           </div>
           <div class="tooltip tooltip-left" data-tip="Contact">
-            <LucideContact :size="22" class="text-accent" />
+            <LucideContact :size="18" class="text-accent" />
           </div>
         </div>
       </div>
-      <div class="drawer-content fixed">
-        <!-- Page content here -->
-        <label for="my-drawer-4" class="btn btn-circle btn-outline mt-8">
-          <lucideMenu :size="22" />
-        </label>
-      </div>
     </div>
+    <!-- sidebar toggle -->
+    <div class="md:hidden drawer-content fixed right-0 mt-8">
+      <label for="my-drawer-4" class="btn btn-circle btn-outline">
+        <lucideMenu :size="18" />
+      </label>
   </div>
-
+  </div>
   <div class="drawer drawer-end">
     <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,8 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 }
 
