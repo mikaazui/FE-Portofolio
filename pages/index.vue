f3583629797cf6a91f4c06a87f27cb37d679105d
mikaazui <adhika725@gmail.com>
2024-01-16 09:03:19 +0700

    install daisy UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>ini index</div>
+  <div class="flex-row justify-center p-16 h-screen">
+    <h1 class="font-semibold text-slate-400 text-6xl">Index Page.</h1>
+    <button @click="clickMe" class="btn mt-5">Click Me!</button>
+
+  </div>
 </template>
 
 <script>
 export default {
   setup() {
-    return {};
+    const clickMe = () => {
+      alert("you clicked me!");
+    };
+    return {clickMe};
   },
 };
 </script>
