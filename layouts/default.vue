e59ffb30702450c90313573cdc11e52f2da3f856
mikaazui <adhika725@gmail.com>
2024-01-17 10:10:20 +0700

    update scroll behaivour

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,128 +1,145 @@
 <template>
- <div class="min-h-screen flex text-accent">
-  <!-- MAIN CONTENT -->
-  <div class="min-h-screen grow grid grid-cols-10">
+  <div class="min-h-screen flex text-accent">
+    <!-- MAIN CONTENT -->
     <!-- PROFILE CARD -->
     <slot />
-  </div>
-  <!-- SIDEMENU -->
-  <div class="min-h-screen w-28 flex-none max-md:hidden">
-    <div class="fixed min-h-screen flex items-center">
-      <div
-        class="border-2 border-neutral-600 p-5 rounded-full flex flex-col gap-5"
-      >
-        <div class="tooltip tooltip-left" data-tip="Home">
-          <LucideHome :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="About">
-          <LucideUserRound :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="Resume">
-          <LucideBriefcase :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="Service">
-          <LucideLibraryBig :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="Skills">
-          <LucideBlocks :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="Portofolio">
-          <LucideNotebook-tabs :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="Testimonials">
-          <LucideMessageSquareText :size="18" class="text-secondary" />
-        </div>
-        <div class="tooltip tooltip-left" data-tip="Contact">
-          <LucideContact :size="18" class="text-secondary" />
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- sidebar toggle -->
-  <div class="md:hidden drawer-content fixed right-0 mt-8">
-    <label for="my-drawer-4" class="btn btn-circle btn-outline">
-      <lucideMenu :size="18" />
-    </label>
-  </div>
-</div>
-<div class="drawer drawer-end">
-  <input
-    id="my-drawer-4"
-    type="checkbox"
-    class="drawer-toggle"
-  />
-
-  <div class="drawer-side overflow-x-hidden text-accent">
-    <label
-      for="my-drawer-4"
-      aria-label="close sidebar"
-      class="drawer-overlay"
-    ></label>
-    <div class="p-14 w-80 min-h-full bg-base-200">
-      <div class="text-2xl mb-4 text-white">Menu</div>
-      <ul class="flex-col flex gap-4">
-        <!-- Sidebar content here -->
-        <li>
-          <a class="flex items-center gap-3">
-            <LucideHome :size="18" class="text-secondary" />
-            <div>Home</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
+    <!-- SIDEMENU -->
+    <div class="min-h-screen w-28 flex-none max-md:hidden">
+      <div class="fixed min-h-screen flex items-center">
+        <div
+          class="border-2 border-neutral-600 p-5 rounded-full flex flex-col gap-5"
+        >
+          <NuxtLink
+            :to="{ path: '/', hash: '#about' }"
+            class="tooltip tooltip-left"
+            data-tip="About"
+          >
             <LucideUserRound :size="18" class="text-secondary" />
-            <div>About</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
+          </NuxtLink>
+          <NuxtLink
+            :to="{ path: '/', hash: '#resume' }"
+            class="tooltip tooltip-left"
+            data-tip="Resume"
+          >
             <LucideBriefcase :size="18" class="text-secondary" />
-            <div>Resume</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
-            <LucideLibraryBig :size="18" class="text-secondary" />
-            <div>Service</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
+          </NuxtLink>
+          <NuxtLink
+            :to="{ path: '/', hash: '#skills' }"
+            class="tooltip tooltip-left"
+            data-tip="Skills"
+          >
             <LucideBlocks :size="18" class="text-secondary" />
-            <div>Skills</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
+          </NuxtLink>
+          <NuxtLink
+            :to="{ path: '/', hash: '#project' }"
+            class="tooltip tooltip-left"
+            data-tip="Project"
+          >
             <LucideNotebook-tabs :size="18" class="text-secondary" />
-            <div>Portofolio</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
+          </NuxtLink>
+          <NuxtLink
+            :to="{ path: '/', hash: '#testimonials' }"
+            class="tooltip tooltip-left"
+            data-tip="Testimonials"
+          >
             <LucideMessageSquareText :size="18" class="text-secondary" />
-            <div>Testimonials</div>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center gap-3">
+          </NuxtLink>
+          <NuxtLink
+            :to="{ path: '/', hash: '#contact' }"
+            class="tooltip tooltip-left"
+            data-tip="Contact"
+          >
             <LucideContact :size="18" class="text-secondary" />
-            <div>Contact</div>
-          </a>
-        </li>
-      </ul>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+    <!-- sidebar toggle -->
+    <div class="md:hidden drawer-content fixed right-0 mt-8 mr-8">
+      <label for="my-drawer-4" class="btn btn-circle btn-outline">
+        <lucideMenu :size="18" />
+      </label>
+    </div>
+  </div>
+  <div class="drawer drawer-end">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
 
-      <!-- social -->
-      <div>
-        <div class="text-2xl my-4 text-white">social</div>
-        <div class="flex gap-4">
-          <lucideInstagram :size="18" class="text-secondary" />
-          <lucideGithub :size="18" class="text-secondary" />
-          <lucideTwitter :size="18" class="text-secondary" />
+    <div class="drawer-side overflow-x-hidden text-accent">
+      <label
+        for="my-drawer-4"
+        aria-label="close sidebar"
+        class="drawer-overlay"
+      ></label>
+      <div class="p-14 w-80 min-h-full bg-base-200">
+        <div class="text-2xl mb-4 text-white">Menu</div>
+        <ul class="flex-col flex gap-4">
+          <!-- Sidebar content here -->
+          <li>
+            <NuxtLink
+              :to="{ path: '/', hash: '#about' }"
+              class="flex items-center gap-3"
+            >
+              <LucideUserRound :size="18" class="text-secondary" />
+              <div>About</div>
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              :to="{ path: '/', hash: '#resume' }"
+              class="flex items-center gap-3"
+            >
+              <LucideBriefcase :size="18" class="text-secondary" />
+              <div>Resume</div>
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              :to="{ path: '/', hash: '#skills' }"
+              class="flex items-center gap-3"
+            >
+              <LucideBlocks :size="18" class="text-secondary" />
+              <div>Skills</div>
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              :to="{ path: '/', hash: '#project' }"
+              class="flex items-center gap-3"
+            >
+              <LucideNotebook-tabs :size="18" class="text-secondary" />
+              <div>Project</div>
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              :to="{ path: '/', hash: '#testiomonials' }"
+              class="flex items-center gap-3"
+            >
+              <LucideMessageSquareText :size="18" class="text-secondary" />
+              <div>Testimonials</div>
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              :to="{ path: '/', hash: '#contact' }"
+              class="flex items-center gap-3"
+            >
+              <LucideContact :size="18" class="text-secondary" />
+              <div>Contact</div>
+            </NuxtLink>
+          </li>
+        </ul>
+
+        <!-- social -->
+        <div>
+          <div class="text-2xl my-4 text-white">social</div>
+          <div class="flex gap-4">
+            <lucideInstagram :size="18" class="text-secondary" />
+            <lucideGithub :size="18" class="text-secondary" />
+            <lucideTwitter :size="18" class="text-secondary" />
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,78 @@
     </div>
     <!-- SECTIONS -->
     <div class="col-span-10 md:col-span-7">
-      <div class="min-h-screen"></div>
-      <div class="min-h-screen"></div>
-      <div class="min-h-screen"></div>
-      <div class="min-h-screen"></div>
+      <div class="min-h-screen" id="about">
+        <span>about</span>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          expedita veniam, unde voluptates velit consectetur! Ab facilis
+          voluptatem optio, repellat iure sint sequi quod ipsa distinctio vel
+          voluptates ex nam, ut aperiam mollitia! Harum, vitae ad, architecto
+          repellendus a vel deserunt voluptatem inventore in esse sit
+          laudantium, earum hic culpa!
+        </div>
+      </div>
+      <div class="min-h-screen" id="resume">
+        <span>resume</span>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          accusamus, deserunt, pariatur, obcaecati enim repellat fugiat eveniet
+          cum corrupti nobis sequi! Numquam delectus tenetur, explicabo error
+          veritatis iste mollitia placeat, nihil blanditiis eos sequi! Quidem
+          quisquam, nostrum officia veritatis, sapiente modi optio autem, saepe
+          sunt nisi enim et deserunt nemo?
+        </div>
+      </div>
+      <div class="min-h-screen" id="skills">
+        <span>skills</span>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          quo at beatae quaerat excepturi ullam, temporibus pariatur odit, vero
+          ipsum veniam ipsam atque nobis. Necessitatibus fugiat, labore quis
+          expedita aut, molestias debitis iste, laudantium explicabo ex incidunt
+          eaque enim at iure dolores rem est harum delectus itaque.
+          Voluptatibus, deserunt eveniet?
+        </div>
+      </div>
+      <div class="min-h-screen" id="project">
+        <span>project</span>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, quas
+          nesciunt deserunt necessitatibus laborum officiis doloribus, non,
+          dolore id optio illum omnis atque beatae? Sunt repellendus sed quam
+          dignissimos laboriosam nobis quaerat ab eos inventore dolores repellat
+          repudiandae sit, commodi sapiente incidunt cum quod odit ad
+          reprehenderit reiciendis. Cum, asperiores.
+        </div>
+      </div>
+      <div class="min-h-screen" id="testimonials">
+        <span>testimonials</span>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          expedita veniam, unde voluptates velit consectetur! Ab facilis
+          voluptatem optio, repellat iure sint sequi quod ipsa distinctio vel
+          voluptates ex nam, ut aperiam mollitia! Harum, vitae ad, architecto
+          repellendus a vel deserunt voluptatem inventore in esse sit
+          laudantium, earum hic culpa!
+        </div>
+      </div>
+      <div class="min-h-screen" id="contact">
+        <span>contact</span>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          expedita veniam, unde voluptates velit consectetur! Ab facilis
+          voluptatem optio, repellat iure sint sequi quod ipsa distinctio vel
+          voluptates ex nam, ut aperiam mollitia! Harum, vitae ad, architecto
+          repellendus a vel deserunt voluptatem inventore in esse sit
+          laudantium, earum hic culpa!
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+</style>
