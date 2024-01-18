a6e90fcd2ba935689aa6fb5eae5b4762ceb43b02
mikaazui <adhika725@gmail.com>
2024-01-18 10:09:10 +0700

    update about

--- components/index/about.vue
+++ components/index/about.vue
@@ -1,19 +1,21 @@
 <template>
-  <div>
-    <span>about</span>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          expedita veniam, unde voluptates velit consectetur! Ab facilis
-          voluptatem optio, repellat iure sint sequi quod ipsa distinctio vel
-          voluptates ex nam, ut aperiam mollitia! Harum, vitae ad, architecto
-          repellendus a vel deserunt voluptatem inventore in esse sit
-          laudantium, earum hic culpa!
-        </div>
+  <div class="min-h-screen py-16">
+    <div
+      class="border border-neutral items-center gap-1 w-fit flex rounded-full py-1 px-4"
+    >
+      <lucideUserRound :size="18" class="text-accent" />
+      <div>About</div>
+    </div>
+      <div class="text-3xl font-semibold text-white mb-4">Every great design begin with an even better story</div>
+      <div class="text-white font-light opacity-60">
+        Since beginning my journey as a freelance designer nearly 8 years ago,
+        I've done remote work for agencies, consulted for startups, and
+        collaborated with talented people to create digital products for both
+        business and consumer use. I'm quietly confident, naturally curious, and
+        perpetually working on improving my chopsone design problem at a time.
       </div>
-      <div class="min-h-screen" id="resume">
-  </div>
+    </div>
+  <div class="min-h-screen" id="resume"></div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
