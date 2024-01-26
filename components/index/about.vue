97a6fad4d7470e3462c4f68114d4dd7ce52152ed
mikaazui <adhika725@gmail.com>
2024-01-26 11:21:26 +0700

    finished about

--- components/index/about.vue
+++ components/index/about.vue
@@ -10,6 +10,7 @@
 		<div class="text-5xl font-semibold mb-5">Say Hi from <span class="text-success">{{profile.firstName}}</span>, Webflow Designer and Developer</div>
 		  <div class="  text-3xl font-semibold mb-4">Every great design begin with an even better story</div>
 		  <div class=" font-light opacity-60">
+			  <!-- TODO pr -->
 			Since beginning my journey as a freelance designer nearly 8 years ago,
 			I've done remote work for agencies, consulted for startups, and
 			collaborated with talented people to create digital products for both
@@ -18,12 +19,14 @@
 		  </div>
 		</div>
 		<div class="flex gap-20 grow items-end pb-20">
-		<div>
-		  <div class="text-success text-4xl font-semibold">10+</div>
-		  <div>YEARS OF <br> EXPERIENCE</div>
+			<div>
+			<!-- TODO pr -->
+			<div class="text-success text-4xl font-semibold">10+</div>
+			<div>YEARS OF <br> EXPERIENCE</div>
 		</div>
 		
 		<div>
+			<!-- TODO pr -->
 		  <div class="text-success text-4xl font-semibold">10+</div>
 		  <div>PROJECTS <br> COMPLEPETED</div>
 		</div>
