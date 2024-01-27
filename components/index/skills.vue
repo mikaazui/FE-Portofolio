69d6ef71da09c8af19733f754b2ebfb053d78d49
mikaazui <adhika725@gmail.com>
2024-01-27 11:28:22 +0700

    done calculate projects completed and experiences

--- components/index/skills.vue
+++ components/index/skills.vue
@@ -6,7 +6,7 @@
       <lucideBlocks :size="18" class="text-accent" />
       <div class="uppercase text-sm font-light">Skills</div>
     </div>
-    <div class="mt-5">
+    <div class="mt-5"> 
       <div class="text-2xl md:text-4xl font-semibold text-accent mb-5">
         Every great Projects begin with Wilingness to learn
       </div>
