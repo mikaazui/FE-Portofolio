6cc3a5a3efa1f836ccae164810e49bba60cdef8a
mikaazui <adhika725@gmail.com>
2024-01-27 12:14:09 +0700

    handle readable date (project)

--- components/index/project.vue
+++ components/index/project.vue
@@ -11,23 +11,31 @@
       <!-- list Project -->
       <div class="md:grid md:grid-cols-2 flex flex-col gap-4">
         <!-- project items -->
-        <div class="col-span-1 md:col-span-1 group" v-for="(pj, i) in projects" :key="i">
-          <div class="flex items-end justify-between">
-            <div class="text-xl font-semibold text-accent">Project Title</div>
-            <div class="font-semibold">02, 08, 2023</div>
-          </div>
-          <!-- TODO create photo -->
-          <div
-            class="aspect-video bg-neutral rounded-xl mt-2 group-hover:scale-105 duration-200"
-          ></div>
-          <div class="flex gap-2 max-md:no-scrollbar overflow-x-auto mt-2">
-            <!-- TODO make skill relationship -->
+        <div
+          class="col-span-1 md:col-span-1 group"
+          v-for="(pj, i) in projects"
+          :key="i"
+        >
+          <div class="flex flex-col">
+            <div class="flex-none items-end justify-between grow">
+              <div class="text-xl font-semibold text-accent">
+                {{ pj.title }}
+              </div>
+              <div class="font-light opacity-60">{{pj.shortDate}}</div>
+            </div>
+            <!-- TODO create photo -->
             <div
-              v-for="n in 6"
-              :key="n"
-              class="rounded-xl border border-neutral text-nowrap px-2"
-            >
-              Skill {{ n }}
+              class="aspect-video bg-neutral rounded-xl mt-2 group-hover:scale-105 duration-200"
+            ></div>
+            <div class="flex gap-2 max-md:no-scrollbar overflow-x-auto mt-2">
+              <!-- TODO make skill relationship -->
+              <div
+                v-for="n in 6"
+                :key="n"
+                class="rounded-xl border border-neutral text-nowrap px-2"
+              >
+                Skill {{ n }}
+              </div>
             </div>
           </div>
         </div>
