e246558e040b452a8ab1195291e95c3f63492acf
mikaazui <adhika725@gmail.com>
2024-02-03 11:38:34 +0700

    create projectThumbnail component

--- components/index/project.vue
+++ components/index/project.vue
@@ -11,47 +11,9 @@
       <!-- list Project -->
       <div class="md:grid md:grid-cols-2 flex flex-col gap-4">
         <!-- project items -->
-        <div
-          class="col-span-1 md:col-span-1 group"
-          v-for="(pj, i) in projects"
-          :key="i"
-        >
-          <div class="flex flex-col">
-            <div class="flex-none items-end justify-between grow">
-              <div class="text-xl font-semibold text-accent truncate">
-                {{ pj.title }}
-              </div>
-              <!-- TODO BENERIN NTAR -->
-              <div class="font-light opacity-60">
-                {{ pj.readableStartDate }}
-              </div>
-            </div>
-            <!-- TODO create photo -->
-            <div
-              v-if="pj.photos.length"
-              class="overflow-hidden group-hover:scale-105 duration-200 aspect-video rounded-xl mt-2"
-            >
-              <img
-                :src="apiUri + pj.photos[0].path"
-                alt="pj.title"
-                class="object-cover"
-              />
-            </div>
-            <div
-              v-else
-              class="aspect-video bg-neutral rounded-xl mt-2 group-hover:scale-105 duration-200"
-            ></div>
-            <div class="flex gap-2 max-md:no-scrollbar overflow-x-auto mt-2">
-              <!-- TODO make skill relationship -->
-              <div
-                v-for="skills in pj.skills"
-                class="rounded-xl border border-neutral text-nowrap px-2"
-              >
-                {{ skills.title }}
-              </div>
-            </div>
-          </div>
-        </div>
+      <template v-for="(pj, i) in projects">
+      <ProjectThumbnail :project="pj" />
+      </template>
       </div>
       <NuxtLink to="/project" class="btn btn-primary w-full mt-5">See All Projects</NuxtLink>
     </div>
@@ -62,8 +24,6 @@
 const props = defineProps({
   projects: Array,
 });
-const config = useRuntimeConfig();
-const apiUri = config.public.apiUri;
 </script>
 
 <style></style>
