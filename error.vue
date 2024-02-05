4297ea2f4e68599f952522609a69cbaa63ccf8f7
mikaazui <adhika725@gmail.com>
2024-02-05 12:42:50 +0700

    in process to make custom error page

---
+++ error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+        <div class="flex flex-col items-center p-16">
+            <div class="font-bold text-4xl">Oops...</div>
+            <div class="flex gap-3">
+                <div class="font-bold text-3xl">{{ error.statusCode }}</div>
+                <div class="font-bold text-3xl">{{ error.statusMessage }}</div>
+            </div>
+            <ImagesBottle class="opacity-90" />
+            <NuxtLink to="/" class="text-xl font-semibold btn btn-primary">Back To Home</NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    error: Object
+});
+
+</script>
