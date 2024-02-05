b17fcc0d612fe3503c8e83249e6a00bc5a5b1fd3
mikaazui <adhika725@gmail.com>
2024-02-05 09:36:59 +0700

    done updating [id].vur (project)

--- pages/project/[id].vue
+++ pages/project/[id].vue
@@ -1,57 +1,79 @@
 <template>
-  <div class="px-16">
-    <!-- header -->
-    <div class="flex justify-between my-4 border-b border-b-neutral pt-6 max-md:pt-20 pb-3">
-      <div class="text-4xl font-bold">FULLNAME</div>
-      <NuxtLink to="/project">Project</NuxtLink>
+    <div class="px-16">
+        <div class="flex justify-between my-4 border-b border-b-neutral pt-6 max-md:pt-20 pb-3">
+            <div class="text-4xl font-bold">FULLNAME</div>
+            <NuxtLink to="/project">Project</NuxtLink>
 
-    </div>
+        </div>
+        <!-- content project -->
+        <div class="grid grid-cols-10 gap-6 items-center">
+            <!-- sebelah kanan -->
 
-    <!-- content -->
-    <!-- <div class="my-4">{{ blog.content }}</div> -->
-    <!-- photos -->
-    <div>
-      <Carousel :items-to-show="2" :wrapAround="true" :autoplay="3000">
-        <Slide v-for="photo in project.photos" :key="photo.id">
-          <!-- <div class="aspect-video w-full bg-gray-400 text-black test-4xl flex">
-            {{ slide }}
-            
-          </div>  -->
-          <div class="w-full h-full">
-            <div class="justify-center bg-contain">
-              <img v-for="photo in project.photos" :src="apiUri + photo.path" class="w-[600px] h-full rounded-xl px-2"
-                alt="" />
+            <div class="col-span-full md:col-span-7">
+                <!-- photos -->
+                <div>
+                    <Carousel :items-to-show="2" :wrapAround="true" :autoplay="3000">
+                        <Slide v-for="photo in project.photos" :key="photo.id">
+                            <div class="w-full h-full">
+                                <div class="justify-center bg-contain">
+                                    <img v-for="photo in project.photos" :src="apiUri + photo.path"
+                                        class="w-[600px] h-full rounded-xl px-2" alt="" />
+                                </div>
+                            </div>
+                        </Slide>
+
+                        <template #addons>
+                            <Navigation />
+                            <Pagination />
+                        </template>
+                    </Carousel>
+                </div>
+
             </div>
-          </div>
-        </Slide>
+            <!-- sebelah kiri -->
+            <div class="col-span-full md:col-span-3 bg-base-300 p-6 rounded-xl">
+                <!-- data detail -->
+                <!-- content -->
+                <div class="flex items-center">
+                    <div class="text-3xl font-bold text-success border-b border-b-neutral">{{ project.title }}</div>
+                    <div class="font-bold p-2 bg-base-200 rounded-xl flex justify-center">{{ status }}</div>
+                </div>
 
-        <template #addons>
-          <Navigation />
-          <Pagination />
-        </template>
-      </Carousel>
-    </div>
-    <div class="flex items-center">
-      <div class="text-3xl font-bold text-success border-b border-b-neutral">{{ project.title }}</div>
-      <div class="badge-primary rounded-3xl font-bold p-1.5 mx-3">{{ status }}</div>
+                <div class="mt-4">
+                    <div class="text-lg font-semibold">Date: {{ project.readableStartDate }} - {{ project.readableEndDate ?
+                        project.readableEndDate : "Present " }}</div>
+                    <div class="font-bold">Company: {{ project.company ? project.company : 'No Company' }}
+                    </div>
+                </div>
+                <template v-if ="project.skills">
+                    <!-- skills -->
+                    <div class="flex gap-2 auto mt-3">
+                        <div v-for="skills in project.skills" class="rounded-xl badge badge-outline badge-success text-nowrap px-2">
+                            {{ skills.title }}
+                        </div>
+                    </div>
+                </template>
+            </div>
+        </div>
 
-    </div>
+        <div class="">
+            <div class="my-4 justify-center">{{ project.description }}</div>
+            <div class="my-4 justify-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur
+                voluptatibus aspernatur ipsam, aperiam error veniam cumque aut in corrupti placeat ea libero quia. Magnam culpa
+                dolorum totam dicta, veritatis dolor numquam! Repudiandae dicta, id sapiente rerum autem neque! Repudiandae
+                tempora
+                incidunt nulla soluta neque perspiciatis unde rem ea molestias dolores ipsa in consequuntur veniam, suscipit
+                laboriosam. Itaque dolores voluptates ratione illo fuga, quisquam praesentium qui, et obcaecati corporis iste
+                ab?
+                Veritatis cum nihil totam doloribus delectus mollitia labore corrupti nulla voluptatibus nam voluptatem
+                possimus,
+                obcaecati cupiditate praesentium. Aspernatur atque aut odit nihil autem, suscipit corporis voluptatum
+                perferendis
+                amet ipsa?</div>
+        </div>
+        </div>
+        <!-- header -->
 
-    <div class="flex items-center my-3 gap-3">
-      <div class="text-lg font-semibold"> {{ project.readableStartDate }} - {{ project.readableEndDate ?
-        project.readableEndDate : "Present " }}</div>
-      <div class="font-bold badge-success p-1.5 rounded-3xl">{{ project.company ? project.company : 'No Company' }}</div>
-    </div>
-    <div class="my-4 justify-center">{{ project.description }}</div>
-    <div class="my-4 justify-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam tenetur
-      voluptatibus aspernatur ipsam, aperiam error veniam cumque aut in corrupti placeat ea libero quia. Magnam culpa
-      dolorum totam dicta, veritatis dolor numquam! Repudiandae dicta, id sapiente rerum autem neque! Repudiandae tempora
-      incidunt nulla soluta neque perspiciatis unde rem ea molestias dolores ipsa in consequuntur veniam, suscipit
-      laboriosam. Itaque dolores voluptates ratione illo fuga, quisquam praesentium qui, et obcaecati corporis iste ab?
-      Veritatis cum nihil totam doloribus delectus mollitia labore corrupti nulla voluptatibus nam voluptatem possimus,
-      obcaecati cupiditate praesentium. Aspernatur atque aut odit nihil autem, suscipit corporis voluptatum perferendis
-      amet ipsa?</div>
-  </div>
 </template>
 
 <script setup>
@@ -64,7 +86,7 @@ const config = useRuntimeConfig();
 const apiUri = config.public.apiUri;
 //modify status string
 const status = computed(() => {
-  return project.status.replaceAll("_", " ")
+    return project.status.replaceAll("_", " ")
 })
 
 
