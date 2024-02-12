312c2041e7f49142c57b349f47d37ce942a8463f
mikaazui <adhika725@gmail.com>
2024-02-12 10:04:19 +0700

    update navigation and page

--- components/adminLayout/navigation.vue
+++ components/adminLayout/navigation.vue
@@ -1,23 +1,23 @@
 <template>
-    <ul class="menu p-4 w-64 min-h-full text-base-content side-menu max-lg:bg-base-200">
-      <div class="px-6 py-4 font-light text-xl">Main Menu</div>
-        <template v-for="menu in menus">
-            <AdminLayoutNavItems :menu="menu" />
-        </template>
-    </ul>
+  <ul class="menu p-4 w-64 min-h-full text-base-content side-menu max-lg:bg-base-200">
+    <div class="px-6 py-4 font-light text-xl">Main Menu</div>
+    <template v-for="menu in menus">
+      <AdminLayoutNavItems :menu="menu" />
+    </template>
+  </ul>
 </template>
 
 <script setup>
 const menus = [
   {
-    title: "Home",
-    to:"/admin",
-    icon: resolveComponent("LucideHome"),
-    children: [{
-      title: "Dashboard",
-      to: "/admin",
-      icon: resolveComponent("LucideLayoutDashboard"),
-    }]
+    title: "Dashboard",
+    to: "/admin",
+    icon: resolveComponent("LucideLayoutDashboard"),
+  },
+  {
+    title: "Project",
+    to: "/admin/project",
+    icon: resolveComponent("LucideNotebook-tabs"),
   }
 ];
 
