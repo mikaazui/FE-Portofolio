c2172bdff9502cda00b941994604eb6bb0506aef
mikaazui <adhika725@gmail.com>
2024-02-12 10:31:42 +0700

    add module: pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ["nuxt-lucide-icons"],
+  modules: ["nuxt-lucide-icons", "@pinia/nuxt"],
   runtimeConfig: {
     apiUri: "http://localhost:5000", // can be overridden by NUXT_API_SECRET environment variable
     public: {
