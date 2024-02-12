d188f5d1bba3b58eb1d298ae7fd1d28fe40109ae
mikaazui <adhika725@gmail.com>
2024-02-12 09:55:55 +0700

    creeate menu navigation component

--- components/adminLayout/navigation.vue
+++ components/adminLayout/navigation.vue
@@ -1,55 +1,24 @@
 <template>
     <ul class="menu p-4 w-64 min-h-full text-base-content side-menu max-lg:bg-base-200">
-        <!-- Sidebar content here -->
-        <div class="text-xl px-4 mb-2 pb-2 pt-2 font-light border-b border-slate-300">Main Menu</div>
-        <li>
-            <details closed>
-                <summary>
-                    <LucideLayoutDashboard :size="18" />
-                    Dashboard
-                </summary>
-                <ul>
-                    <template v-for="menu in menus">
-                        <li>
-                            <NuxtLink to="menu.to">
-                                <component :is="menu.icon" :size="18" />{{ menu.title }}
-                                <!-- <LucideLayoutDashboard :size="18" /> -->
-                            </NuxtLink>
-                        </li>
-                    </template>
-                </ul>
-            </details>
-        </li>
-
+      <div class="px-6 py-4 font-light text-xl">Main Menu</div>
+        <template v-for="menu in menus">
+            <AdminLayoutNavItems :menu="menu" />
+        </template>
     </ul>
 </template>
 
 <script setup>
 const menus = [
-    {
-        title: "Home",
-        to: "/admin",
-        icon: resolveComponent('LucideHome'),
-    },
-    {
-        title: "Home",
-        to: "/admin",
-        icon: resolveComponent('LucideHome'),
-    },
-    {
-        title: "Home",
-        to: "/admin",
-        icon: resolveComponent('LucideHome'),
-        children: [
-            {
-                title: "Home",
-                to: "/admin",
-                icon: resolveComponent('LucideHome'),
-            },
-
-        ]
-    },
-
-]
+  {
+    title: "Home",
+    to:"/admin",
+    icon: resolveComponent("LucideHome"),
+    children: [{
+      title: "Dashboard",
+      to: "/admin",
+      icon: resolveComponent("LucideLayoutDashboard"),
+    }]
+  }
+];
 
 </script>
