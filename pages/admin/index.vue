ff5875cd1ebf2c519200353f8cd5a6a78471111d
mikaazui <adhika725@gmail.com>
2024-02-12 11:38:17 +0700

    solvoing problem

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,10 +2,17 @@
     <div>
         ini halaman index
     </div>
+    <div>
+        {{ CounterStore.count }}
+    </div>
+    <button class="btn btn-primary" @click="CounterStore.increment">add counter</button>
+    <button class="btn btn-primary" @click="CounterStore.decrement">add counter</button>
 </template>
 
 <script setup>
 definePageMeta({
     layout: 'admin'
 })
+import { useCounterStore } from '../../stores/counterStore';
+const CounterStore = useCounterStore(); 
 </script>
