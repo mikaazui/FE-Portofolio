28edd15c12e4210aaf054764341e1b77ca6687e5
mikaazui <adhika725@gmail.com>
2024-02-15 11:52:02 +0700

    make the theme persistent (remove flickering)

--- components/defaultlayout/themeToggle.vue
+++ components/defaultlayout/themeToggle.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
-    <label class="swap swap-rotate" :v-model="colorMode.preference">
-
+    <label class="swap swap-rotate">
       <!-- this hidden checkbox controls the state -->
-      <input type="checkbox" class="theme-controller" value="light" />
-
+      <input type="checkbox" class="theme-controller" value="light" v-model="checked" />
       <!-- sun icon -->
       <LucideSun class="swap-on" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" />
       <!-- moon icon -->
       <LucideMoon class="swap-off" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" />
-
     </label>
   </div>
 </template>
 
-
 <script setup>
 const colorMode = useColorMode();
+const checked = ref(colorMode.preference === 'dark');
+
+// Watch for changes in the checked value and update the color mode accordingly
+watch(checked, (value) => {
+  const newPreference = value ? 'dark' : 'light';
+  colorMode.preference = newPreference;
+  localStorage.setItem('colorMode', newPreference);
+});
+
+// Load theme preference from localStorage when the component is mounted
+onMounted(() => {
+  const storedColorMode = localStorage.getItem('colorMode');
+  if (storedColorMode) {
+    checked.value = storedColorMode === 'dark';
+  }
+});
 </script>
 
 <style></style>
