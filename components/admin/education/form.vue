a1e41b9438db2b9eedb233fe89eae700cc434165
mikaazui <adhika725@gmail.com>
2024-03-01 20:15:32 +0700

    in process of  / updating form.vue (merging update)

--- components/admin/education/form.vue
+++ components/admin/education/form.vue
@@ -1,176 +1,193 @@
 <template>
-  <!-- modal -->
-  <input type="checkbox" v-model="show_modal" class="modal-toggle" />
-  <div class="modal" role="dialog">
-    <div class="modal-box">
-      <form method="dialog">
-        <label @click="$emit('close')" class="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">✕</label>
-      </form>
-      <h1 class="text-xl font-semibold">Add Education</h1>
+   <!-- modal -->
+   <input type="checkbox" v-model="show_modal" class="modal-toggle" />
+   <div class="modal" role="dialog">
+      <div class="modal-box">
+         <form method="dialog">
+            <label @click="$emit('close')" class="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">✕</label>
+         </form>
+         <h1 class="text-xl font-semibold"> {{ data ? `Update ${data.insituitionName}` : 'Add Education' }} </h1>
 
-      <label class="form-control w-full max-w-xs">
-        <div class="label">
-          <span class="label-text">institution Name</span>
-        </div>
-        <input v-model="formData.insituitionName" type="text" placeholder="institution Name"
-          class="input input-bordered w-full max-w-xs" />
-        <div class="text-xs text-right text-error" v-if="errors.insituitionName">{{ errors.insituitionName }}</div>
-      </label>
+         <label class="form-control w-full max-w-xs">
+            <div class="label">
+               <span class="label-text">institution Name</span>
+            </div>
+            <input v-model="formData.insituitionName" type="text" placeholder="institution Name"
+               class="input input-bordered w-full max-w-xs" />
+            <div class="text-xs text-right text-error" v-if="errors.insituitionName">{{ errors.insituitionName }}</div>
+         </label>
 
-      <label class="form-control w-full max-w-xs">
-        <div class="label">
-          <span class="label-text">Major</span>
-        </div>
-        <input v-model="formData.major" type="text" placeholder="Major" class="input input-bordered w-full max-w-xs" />
-        <div class="text-xs text-right text-error" v-if="errors.major">{{ errors.major }}</div>
-      </label>
+         <label class="form-control w-full max-w-xs">
+            <div class="label">
+               <span class="label-text">Major</span>
+            </div>
+            <input v-model="formData.major" type="text" placeholder="Major" class="input input-bordered w-full max-w-xs" />
+            <div class="text-xs text-right text-error" v-if="errors.major">{{ errors.major }}</div>
+         </label>
 
-      <label class="form-control w-full max-w-xs">
-        <div class="label">
-          <span class="label-text">Degree</span>
-        </div>
-        <input v-model="formData.degree" type="text" placeholder="Degree" class="input input-bordered w-full max-w-xs" />
-        <div class="text-xs text-right text-error" v-if="errors.degree">{{ errors.degree }}</div>
-      </label>
+         <label class="form-control w-full max-w-xs">
+            <div class="label">
+               <span class="label-text">Degree</span>
+            </div>
+            <input v-model="formData.degree" type="text" placeholder="Degree"
+               class="input input-bordered w-full max-w-xs" />
+            <div class="text-xs text-right text-error" v-if="errors.degree">{{ errors.degree }}</div>
+         </label>
 
-      <label class="form-control w-full max-w-xs">
-        <div class="label">
-          <span class="label-text">City</span>
-        </div>
-        <input v-model="formData.city" type="text" placeholder="city" class="input input-bordered w-full max-w-xs" />
-        <div class="text-xs text-right text-error" v-if="errors.city">{{ errors.city }}</div>
-      </label>
+         <label class="form-control w-full max-w-xs">
+            <div class="label">
+               <span class="label-text">City</span>
+            </div>
+            <input v-model="formData.city" type="text" placeholder="city" class="input input-bordered w-full max-w-xs" />
+            <div class="text-xs text-right text-error" v-if="errors.city">{{ errors.city }}</div>
+         </label>
 
-      <label class="form-control w-full max-w-xs">
-        <div class="label">
-          <span class="label-text">Start Year</span>
-        </div>
-        <DatePicker v-model="formData.startYear" color="gray">
-          <template #default="{ togglePopover }">
-            <button @click="togglePopover" class="btn btn-outline border-neutral/90 font-normal">
-              {{ dayjs(formData.startYear).format('D MMMM YYYY') }}
+         <label class="form-control w-full max-w-xs">
+            <div class="label">
+               <span class="label-text">Start Year</span>
+            </div>
+            <DatePicker v-model="formData.startYear" color="gray">
+               <template #default="{ togglePopover }">
+                  <button @click="togglePopover" class="btn btn-outline border-neutral/90 font-normal">
+                     {{ formData.startYear ? dayjs(formData.startYear).format('YYYY') : '2024' }}
+                  </button>
+               </template>
+            </DatePicker>
+            <div class="text-xs text-right text-error" v-if="errors.startYear">{{ errors.startYear }}</div>
+         </label>
+
+         <div class="flex items-center gap-3">
+            <label class="form-control w-full max-w-xs">
+               <div class="label">
+                  <span class="label-text">End Year</span>
+               </div>
+               <DatePicker v-model="formData.endYear" color="gray">
+                  <template #default="{ togglePopover }">
+                     <button @click="togglePopover" class="btn btn-outline border-neutral/90 font-normal"
+                        :disabled="isPresent">
+                        {{ dayjs(formData.endDate).format('YYYY') }}
+                     </button>
+                  </template>
+               </DatePicker>
+               <div class="text-xs text-right text-error" v-if="errors.endYear">{{ errors.endYear }}</div>
+
+            </label>
+            <div class="flex items-center">
+               <!-- TODO CHECKBOXNYA ? JIKA ADA DATA > CHECKBOX TERCENTANG -->
+               <input type="checkbox" v-model="isPresent" @change="handlePresent" class="checkbox" />
+               <label for="" class="label">Present</label>
+            </div>
+
+
+         </div>
+
+         <div class="flex gap-3 py-3 justify-end">
+            <button @click="$emit('close')" class="btn btn-error text-white">Close</button>
+            <button @click="handleSave" class="btn btn-success text-white">
+               {{ data ? 'Update' : 'Save' }}
+               <span v-show="isLoading" class="loading loading-spinner loading-md"></span>
             </button>
-          </template>
-        </DatePicker>
-        <div class="text-xs text-right text-error" v-if="errors.startYear">{{ errors.startYear }}</div>
-      </label>
-
-      <div class="flex items-center gap-3">
-        <label class="form-control w-full max-w-xs">
-          <div class="label">
-            <span class="label-text">End Year</span>
-          </div>
-          <DatePicker v-model="formData.endYear" color="gray">
-            <template #default="{ togglePopover }">
-              <button @click="togglePopover" class="btn btn-outline border-neutral/90 font-normal" :disabled="isPresent">
-                {{ dayjs(formData.endYear).format('D MMMM YYYY') }}
-              </button>
-            </template>
-          </DatePicker>
-          <div class="text-xs text-right text-error" v-if="errors.endYear">{{ errors.endYear }}</div>
-
-        </label>
-        <div class="flex items-center">
-          <input v-model="isPresent" type="checkbox" @change="handlePresent" class="checkbox" />
-          <label for="" class="label">Present</label>
-        </div>
-
-
+            <div class="text-xs text-error">{{ fetchError }}</div>
+         </div>
       </div>
-
-      <div class="flex gap-3 py-3 justify-end">
-        <button @click="$emit('close')" class="btn btn-error text-white">Close</button>
-        <button @click="handleSave" class="btn btn-success text-white">Save</button>
-        <div class="text-xs text-error" >{{ fetchError }}</div>
-      </div>
-    </div>
-  </div>
+   </div>
 </template>
 
 <!-- TODO PINDAHIN METHOD UPDATE KESINI -->
-
-
 
 <script setup>
 import Joi from 'joi';
 import dayjs from 'dayjs'
 import { DatePicker } from 'v-calendar'
 const props = defineProps({
-  show: Boolean,
-  text_confirm: String,
-
+   data: Object,
+   show: Boolean,
 })
 
 const errors = ({})
 const fetchError = ref('')
 const isLoading = ref(false)
-const formData = ref({
-  insituitionName: '',
-  major: '',
-  degree: '',
-  city: '',
-  startYear: '',
-  endYear: ''
-})
+const emits = defineEmits(['close', 'saved'])
+const isPresent = ref(false)
 
-const emits = defineEmits(['close', 'yes', 'saved'])
 const show_modal = ref(false)
-watchEffect(() => {
-  show_modal.value = props.show
+const formData = ref({})
 
-  //reset form
-  formData.value = {
-    insituitionName: '',
-    major: '',
-    degree: '',
-    city: '',
-    startYear: new Date(),
-    endYear: new Date()
-  }
-})
+watchEffect(() => {
+   show_modal.value = props.show
+
+   //reset form
+   formData.value = {
+      insituitionName: props.data ? props.data.insituitionName : '',
+      startYear: props.data ? props.data.startYear : '',
+      endYear: props.data ? props.data.endYear : '',
+      major: props.data ? props.data.major : '',
+      degree: props.data ? props.data.degree : ''
+      //kondisi 1 props.data null
+      //kondisi 2 props.data.endDate = null
+
+   };
+
+   if (props.data) {
+      // tergantung kondisi endDate
+      isPresent.value = props.data.endDate == null;
+   } else {
+      // default
+      isPresent.value = false;
+   }
+
+});
 
 const EduStore = useEducationStore();
 const handleSave = async () => {
-  isLoading.value = true
-  try {
-    errors.value = {}
-    fetchError.value = ''
-    console.log('handle save')
-    console.log(formData.value)
-    //ubah data endYEar jika kosong
-    if(isPresent.value) formData.value.endYear = null
+   isLoading.value = true;
+   try {
 
-    await EduStore.create(formData.value)
-    show_modal.value = false
-    isLoading.value = false
-    emits('saved')
+      // jika isPresent ter-centang
+      if (isPresent.value) {
+         // ubah endDate menjadi null
+         formData.value.endDate = null;
+      }
 
-  } catch (error) {
-    isLoading.value = false
-    if (error instanceof Joi.ValidationError) {
-      //joi error
-      errors.value = joiError(error)
-      console.log(errors.value)
-    } else {
-      if (error.data) {
-        //fetch error
-        console.log(error)
-        console.log(error.status)
-        console.log(error.message)
-        fetchError.value = error.message
+      if (!props.data) {
+         // handle create
+         await EduStore.create(formData.value)
+      } else {
+         //update
+         const id = props.data.id;
+         await EduStore.create(id, formData.value)
       }
-      else {
-        console.log('error codingan')
-        console.log(error)
+
+      isLoading.value = false;
+      // emit saved
+      emits('saved')
+   } catch (error) {
+      isLoading.value = false;
+
+      if (error instanceof Joi.ValidationError) {
+         errors.value = joiError(error);
+      } else {
+         if (error.data) {
+            fetchError.value = error.data.message
+         } else {
+            console.log(error)
+         }
       }
-    }
-  }
+   }
 };
 
 const handlePresent = (e) => {
-  isPresent.value = e.target.checked
+   isPresent.value = e.target.checked
+   if (isPresent.value) {
+      formData.value.endYear = null
+   }
 }
-const isPresent = ref(false)
 
+const showForm = ref(false);
+const editData = ref(null);
+
+const saved = () => {
+   console.log('saved')
+}
 
 </script>
