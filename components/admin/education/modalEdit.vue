79cc1a7d57ecbead33e087a47d05a969ba72a697
mikaazui <adhika725@gmail.com>
2024-03-01 20:12:45 +0700

    cleaning up modal edit (i wont be using this anymore)

--- components/admin/education/modalEdit.vue
+++ components/admin/education/modalEdit.vue
@@ -15,43 +15,51 @@
           </div>
           <input v-model="formData.institutionName" type="text" placeholder="institution Name"
             class="input input-bordered w-full max-w-xs" />
-          <div class="text-xs text-right text-error" >{{ errors.institutionName }}</div>
+          <div class="text-xs text-right text-error">{{ errors.institutionName }}</div>
         </label>
         <!--  -->
         <label class="form-control w-full max-w-xs">
           <div class="label">
             <span class="label-text">City</span>
           </div>
-          <input v-model="formData.city" type="text" placeholder="City"
-            class="input input-bordered w-full max-w-xs" />
-          <div class="text-xs text-right text-error" >{{ errors.city }}</div>
+          <input v-model="formData.city" type="text" placeholder="City" class="input input-bordered w-full max-w-xs" />
+          <div class="text-xs text-right text-error">{{ errors.city }}</div>
         </label>
         <!--  -->
         <label class="form-control w-full max-w-xs">
           <div class="label">
             <span class="label-text">Start Year</span>
           </div>
-          <input v-model="formData.startYear" type="text" placeholder="Start Year"
-            class="input input-bordered w-full max-w-xs" />
-          <div class="text-xs text-right text-error" >{{ errors.startYear }}</div>
+          <DatePicker v-model="formData.startYear" color="gray">
+            <template #default="{ togglePopover }">
+              <button @click="togglePopover" class="btn btn-outline border-neutral/90 font-normal">
+                {{ dayjs(formData.startYear).format('YYYY') }}
+              </button>
+            </template>
+          </DatePicker>
+          <div class="text-xs text-right text-error" v-if="errors.startYear">{{ errors.startYear }}</div>
         </label>
         <!--  -->
         <label class="form-control w-full max-w-xs">
           <div class="label">
-            <span class="label-text">End Year Year</span>
+            <span class="label-text">End Year</span>
           </div>
-          <input v-model="formData.endYear" type="text" placeholder="End Year"
-            class="input input-bordered w-full max-w-xs" />
-          <div class="text-xs text-right text-error" >{{ errors.endYear }}</div>
+          <DatePicker v-model="formData.startYear" color="gray">
+            <template #default="{ togglePopover }">
+              <button @click="togglePopover" class="btn btn-outline border-neutral/90 font-normal">
+                {{ dayjs(formData.endYear).format('YYYY') }}
+              </button>
+            </template>
+          </DatePicker>
+          <div class="text-xs text-right text-error" v-if="errors.startYear">{{ errors.startYear }}</div>
         </label>
         <!--  -->
         <label class="form-control w-full max-w-xs">
           <div class="label">
             <span class="label-text">Major</span>
           </div>
-          <input v-model="formData.major" type="text" placeholder="Major"
-            class="input input-bordered w-full max-w-xs" />
-          <div class="text-xs text-right text-error" >{{ errors.major }}</div>
+          <input v-model="formData.major" type="text" placeholder="Major" class="input input-bordered w-full max-w-xs" />
+          <div class="text-xs text-right text-error">{{ errors.major }}</div>
         </label>
         <!--  -->
         <label class="form-control w-full max-w-xs">
@@ -60,7 +68,7 @@
           </div>
           <input v-model="formData.degree" type="text" placeholder="Degree"
             class="input input-bordered w-full max-w-xs" />
-          <div class="text-xs text-right text-error" >{{ errors.degree }}</div>
+          <div class="text-xs text-right text-error">{{ errors.degree }}</div>
         </label>
 
       </div>
@@ -84,6 +92,12 @@ const props = defineProps({
   // text_confirm: String
 });
 
+import dayjs from 'dayjs'
+import { DatePicker } from 'v-calendar'
+
+const formData = ref({})
+const isLoading = ref(false)
+
 const errors = ref({})
 
 const EduStore = useEducationStore();
@@ -91,43 +105,37 @@ const EduStore = useEducationStore();
 const handleUpdate = async () => {
   try {
     isLoading = true;
-    
-    if(!formData.value.endYear) formData.value.endYear = null;
-    
-    if(props.data) {
+
+    if (!formData.value.endYear) formData.value.endYear = null;
+
+    if (props.data) {
       await EduStore.update(props.data.id, formData.value)
       // emits('')
       isLoading = false
     }
-    
-    
+
+
   } catch (error) {
-    
+
   }
 }
 const show_modal = ref(false)
-const formData = ref ({})
-watchEffect (() => {
+watchEffect(() => {
   show_modal.value = props.show
   //reset form
   formData.value = {
-    institutionName: props.data ? props.data.insituitionName : 'Data not found',
-    city: props.data ? props.data.city : 'Data not found',
-    startYear: props.data ? props.data.startYear : 'Data not found',
-    endYear: props.data ? props.data.endYear : 'Data not found',
-    major: props.data ? props.data.major : 'Data not found',
-    degree: props.data ? props.data.degree : 'Data not found',
+    institutionName: props.data ? props.data.insituitionName : '',
+    city: props.data ? props.data.city : '',
+    startYear: props.data ? props.data.startYear : '',
+    endYear: props.data ? props.data.endYear : '',
+    major: props.data ? props.data.major : '',
+    degree: props.data ? props.data.degree : '',
   };
   console.log(formData.value)
-  
+
 })
 
 //TODO HAPUS SEMUANYA, PINDAHIN KE FORM
 
-
-
-  
-
-
-
 </script>
+
