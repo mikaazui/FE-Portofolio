6a28466be907005bf52c38e869fb04193d552e0f
mikaazui <adhika725@gmail.com>
2024-03-01 13:40:26 +0700

    making the mobile view

--- pages/admin/education.vue
+++ pages/admin/education.vue
@@ -1,6 +1,6 @@
 <template>
   <AdminEducationForm :show="addEdu" @close="addEdu = false" @saved="saved" />
-  <AdminEducationModalEdit :show="edit" :data="updateData" @close="edit = false" @yes="handleUpdate" />
+  <AdminEducationModalEdit :show="edit" :data="updateData" @close="edit = false" />
   <AdminEducationRemoveConModal :show="remove" :data="deleteData" @close="remove = false" @yes="handleDelete">
     <div v-if="deleteData" class="pb-3 text-xl font-semibold">Are you sure to delete {{ deleteData.insituitionName }}?
     </div>
@@ -12,13 +12,14 @@
       <div class="flex items-center gap-3">
         <lucideSchool :size="20" class="" />Education
       </div>
-      <button @click="addEdu = true" class="btn btn-md btn-primary ">
+      <button @click="addEdu = true" class="btn btn-md btn-primary max-lg:w-12 flex items-center justify-center">
         <lucidePlus :size="20" />
-        Add Education
+        <div class="max-lg:hidden">Add Education</div>
+
       </button>
     </div>
     <input v-model="filter" type="text" placeholder="Search" class="w-full max-w-xs input input-sm input-bordered" />
-    <div class="overflow-x-auto">
+    <div class="overflow-x-auto max-lg:hidden">
       <table class="table table-zebra">
         <!-- head -->
         <thead>
@@ -33,7 +34,7 @@
           <!-- row 1 -->
           <tr v-for="edu in dataTable" :key="edu.id">
             <td>{{ edu.insituitionName }}</td>
-            <td class="text-center">{{ edu.startYear }} - {{ edu.endYear ? edu.endYear : "Present" }}</td>
+            <td class="text-center">{{ dayjs(edu.startYear).format('D MMMM YYYY') }} - {{ dayjs(edu.endYear).format('D MMMM YYYY') ? dayjs(edu.endYear).format('D MMMM YYYY') : "Present" }}</td>
             <td class="text-center">{{ edu.major ? edu.major : "-" }}</td>
             <td class="text-center">{{ edu.degree ? edu.degree : "-" }}</td>
             <button @click="deleteData = edu; remove = true" class="m-2 btn btn-outline btn-sm btn-circle">
@@ -46,7 +47,57 @@
         </tbody>
       </table>
     </div>
+
+    <div class="lg:hidden py-5">
+      <div v-for="edu in dataTable" :key="edu.id" class="card-body bg-base-200 drop-shadow-lg">
+        <div class="uppercase font-semibold">{{ edu.insituitionName }} </div>
+        <div class="flex items-center justify-between">
+        <div>{{ dayjs(edu.startYear).format('D MMMM YYYY') }} - {{ dayjs(edu.endYear).format('D MMMM YYYY') ?
+            dayjs(edu.endYear).format('D MMMM YYYY') : "Present" }}</div>
+          <div class="flex justify-between gap-2">
+
+            <div class="dropdown dropdown-bottom dropdown-end">
+              <div tabindex="0" role="button" class="btn bg-base-300 m-1">
+                <LucideMoreVertical :size="16" />
+              </div>
+
+              <div class="">
+                <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <a @click="updateData = edu; edit = true" class="rounded-lg p-1.5">
+                      <LucidePen :size="16" />
+                      Edit
+                    </a>
+                  </li>
+                  <li>
+                    <a @click="deleteData = edu; remove = true" class="bg-error rounded-lg p-1.5">
+                      <LucideTrash2 :size="16" />
+                      Delete
+                    </a>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="flex justify-between">
+          <div class="flex items-center justify-between btn btn-accent">
+            <div>Major</div>
+            <div>{{ edu.major }}</div>
+          </div>
+          <div class="flex items-center justify-between btn btn-accent">
+            <div>Degree</div>
+            <div> {{ edu.degree }}</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script setup>
@@ -54,14 +105,16 @@
 definePageMeta({
   layout: 'admin',
   middleware: ['auth'],
-  ssr: true
 })
+import dayjs from 'dayjs'
 
 const edit = ref(false);
 const remove = ref(false);
 const isLoading = (false);
 const success = ref(false);
 const addEdu = ref(false);
+
+
 
 
 const EduStore = useEducationStore();
@@ -88,10 +141,6 @@ const dataTable = computed(() => {
   }
 });
 
-watchEffect(() => {
-  console.log(updateData.value)
-});
-
 const handleDelete = async () => {
   try {
     const id = deleteData.value.id;
@@ -114,17 +163,25 @@ const handleDelete = async () => {
   }
 };
 
-const handleUpdate = async () => {
+const handleSave = async () => {
   try {
-    const id = updateData.value.id;
-    //proses update
-    console.log(id)
-    console.log('masuk method update')
-    
+    isLoading = true;
+
+    if (!formData.value.endYear) formData.value.endYear = null;
+
+    if (!props.data) {
+      //jika gaada >> create
+      await EduStore.create(formData.value)
+    } else {
+      //jika ada >> update
+      await EduStore.update(props.data._id, formData.value)
+    }
+
   } catch (error) {
-    
+
   }
 }
+
 
 const saved = async () => {
   addEdu.value = false
