3a191d986fcc991f6ae79f1d3f22d87d05707374
mikaazui <adhika725@gmail.com>
2024-03-02 15:12:04 +0700

    trying to make delete method at skill

--- stores/skillStore.js
+++ stores/skillStore.js
@@ -14,6 +14,23 @@ export const useSkillStore = defineStore("skills", {
     async getCategories () {
       const Api = useApiStore()
       this.categories = await Api.get('/skill_categories')
+    },
+    async create (data) {
+      const Api = useApiStore()
+      //validasi
+      console.log(data)
+      
+      data = Validate(isSkill, data)
+      console.log('abis validasi')
+      console.log(data)
+      //fetch
+      await Api.post('/skill', data)
+      
+    },
+    async delete (id) {
+      const Api = useApiStore()
+      //delete fetch
+      await Api.delete(`/skill/${id}`)
     }
   }
 });
